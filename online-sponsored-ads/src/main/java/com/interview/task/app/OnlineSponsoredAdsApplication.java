4cf6185a1523e472230ce04605abf0c3ee35b210
galyc <galyc@host.docker.internal>
2021-06-18 15:47:14 +0300

    first commit

---
+++ online-sponsored-ads/src/main/java/com/interview/task/app/OnlineSponsoredAdsApplication.java
@@ -0,0 +1,13 @@
+package com.interview.task.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSponsoredAdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineSponsoredAdsApplication.class, args);
+	}
+
+}
